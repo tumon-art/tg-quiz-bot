1d88be7a2e4d7919a043da0df6424b166327ed55
tumon <mofakhar0@gmail.com>
2024-11-06 15:35:02 +0600

    .

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ const bot = new TelegramBot(process.env.TG_BOT_API, {
 const FILE_PATH = "./src/activeGroups.json";
 let activeGroupIds = new Map<number, string>();
 
+
 // Function to load group IDs and Title from file
 function loadGroupIds() {
   if (fs.existsSync(FILE_PATH)) {
@@ -30,34 +31,30 @@ function saveGroupIds() {
   fs.writeFileSync(FILE_PATH, JSON.stringify(groupArray), "utf8");
 }
 
+
 // Load group IDs when bot starts
 loadGroupIds();
 let currentQuestionIndex = 0;
-let activeGroupId: any = null;
 const interval = 900000;
+// const interval = 20000;
 
-// Listen for the /startquiz command to capture the group ID
+// Only add the group ID if it's a group and hasn't been added before
 bot.onText(/\/startquiz/, (msg: any) => {
   const chatId = msg.chat.id;
+  const chatName = msg.chat.title || "Unnamed Group"; // Use title or fallback
 
-  // Only add the group ID if it's a group and hasn't been added before
-  bot.onText(/\/startquiz/, (msg: any) => {
-    const chatId = msg.chat.id;
-    const chatName = msg.chat.title || "Unnamed Group"; // Use title or fallback
-
-    if (
-      (msg.chat.type === "group" || msg.chat.type === "supergroup") &&
-      !activeGroupIds.has(chatId)
-    ) {
-      activeGroupIds.set(chatId, chatName); // Save chat ID and name
-      saveGroupIds(); // Save to file
-      console.log(`Added group ${chatId} (${chatName}) to active group list`);
-      bot.sendMessage(
-        chatId,
-        "This group is now enrolled to receive quiz questions every 15 minutes!"
-      );
-    }
-  });
+  if (
+    (msg.chat.type === "group" || msg.chat.type === "supergroup") &&
+    !activeGroupIds.has(chatId)
+  ) {
+    activeGroupIds.set(chatId, chatName); // Save chat ID and name
+    saveGroupIds(); // Save to file
+    console.log(`Added group ${chatId} (${chatName}) to active group list`);
+    bot.sendMessage(
+      chatId,
+      "This group is now enrolled to receive quiz questions every 15 minutes!"
+    );
+  }
 });
 
 // Function to send the quiz question to each tracked group
@@ -85,24 +82,4 @@ function sendQuizQuestion() {
 
 // Schedule the quiz to be sent every 15 minutes (900000 ms)
 setInterval(sendQuizQuestion, interval); // 15 minutes
-function msToMinutes(ms: number) {
-  return ms / 60000;
-}
-
-// Example usage:
-const minutes = msToMinutes(interval);
-console.log("interval time:", minutes, "mins");
-
-// Listen for poll answers from users
-bot.on("poll_answer", (msg: any) => {
-  const userAnswerIndex = msg.option_ids[0];
-  const correctAnswerIndex =
-    quizQuestions[currentQuestionIndex].correctOptionId;
-
-  if (userAnswerIndex === correctAnswerIndex) {
-    currentQuestionIndex++;
-    if (activeGroupId) {
-      sendQuizQuestion(); // Send the next question to the group
-    }
-  } else null;
-});
+console.log("interval time:", interval / 60000, "mins");
